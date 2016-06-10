390ee55eb731626b9773185118af347d6972f13d
Marcel Jacobs <silent.party@gmail.com>
2016-06-10 19:08:49 +1000

    added some helpful comments for future reference

--- app.js
+++ app.js
@@ -3,23 +3,33 @@
 // Also got rid of that nasty global variable. Ooops.
 
 function makeNote() {
+  // Get an array of elements matching the selector
     var boxCount = document.getElementsByClassName('box');
+    // Count the elements in boxCount array
     var count = boxCount.length;
-    var box = document.createElement("box", "p"); // Turns out I can create multiple objects here :)
+    // Create the necessary elements. Turns out I can create multiple objects here :)
+    var box = document.createElement("box", "p");
+    // Grab the colour value from user input
     var colourVal = document.getElementById('colour').value;
+    // Grab the text value from user input
     var textVal = document.getElementById('text').value;
+    // Use the current count of box elements to get a number prefix
     var text = document.createTextNode((count + 1) +" - " + textVal);
 
+    // Give the box some class
     box.className = "box";
+    // And a background colour
     box.style.backgroundColor = colourVal;
 
+    // Self explanatory
     document.getElementsByClassName('container')[0].appendChild(box);
+    // Insert the textNode into the current .box element (we know it's the current element because we've counted how many .box elements there are using count, then used count to select the index of the current element)
     document.getElementsByClassName('box')[count].appendChild(text);
 
     return;
 };
 
-document.getElementById('make-sticky').onclick = makeNote;
+document.getElementById('make-sticky').onclick = makeNote; // Make the note
 
 
 /*
