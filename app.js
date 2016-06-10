4569c59c714a158144f111cc444585d3032480a4
Marcel Jacobs <silent.party@gmail.com>
2016-06-10 18:32:57 +1000

    Removed the need for a global variable (actually read the brief, lol)

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
-var count = 0;
 
+// Something a little more modular. We're now freed of the button and can invoke makeNote however we wish.
+// Also got rid of that nasty global variable. Ooops.
+
+function makeNote() {
+    var boxCount = document.getElementsByClassName('box');
+    var count = boxCount.length;
+    var box = document.createElement("box", "p"); // Turns out I can create multiple objects here :)
+    var colourVal = document.getElementById('colour').value;
+    var textVal = document.getElementById('text').value;
+    var text = document.createTextNode((count + 1) +" - " + textVal);
+
+    box.className = "box";
+    box.style.backgroundColor = colourVal;
+
+    document.getElementsByClassName('container')[0].appendChild(box);
+    document.getElementsByClassName('box')[count].appendChild(text);
+
+    console.log(count);
+    return;
+};
+
+document.getElementById('make-sticky').onclick = makeNote;
+
+
+/*
+// Quick n' dirty
+
+var count = 0;
 document.getElementById('make-sticky').onclick = function() {
   var box = document.createElement("box");
   var p = document.createElement("p");
@@ -16,16 +43,4 @@ document.getElementById('make-sticky').onclick = function() {
   count++;
 
 };
-
-
-/*
-
-document.getElementById('make-sticky').onclick = function() {
-
-  var box = document.createElement("box");
-  box.className = "box";
-  document.getElementsByClassName('container')[0].appendChild(box);
-};
-
-
 */
