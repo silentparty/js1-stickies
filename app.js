aa9173c62d7f53a2136a8e84943db4dfdd9d81ae
Marcel Jacobs <silent.party@gmail.com>
2016-06-14 21:10:52 +1000

    now with 100% more instructor feedback

--- app.js
+++ app.js
@@ -4,29 +4,27 @@
 
 function makeNote() {
   // Get an array of elements matching the selector
-    var boxCount = document.getElementsByClassName('box');
-    // Count the elements in boxCount array
-    var count = boxCount.length;
+    var count = document.getElementsByClassName('box').length;
     // Create the necessary elements. Turns out I can create multiple objects here :)
     var box = document.createElement("box", "p");
     // Grab the colour value from user input
-    var colourVal = document.getElementById('colour').value;
+    var noteColour = document.getElementById('colour').value;
     // Grab the text value from user input
-    var textVal = document.getElementById('text').value;
+    var noteMessage = document.getElementById('text').value;
     // Use the current count of box elements to get a number prefix
-    var text = document.createTextNode((count + 1) +" - " + textVal);
+    var textNode = document.createTextNode((count + 1) +" - " + noteMessage);
 
     // Give the box some class
     box.className = "box";
     // And a background colour
-    box.style.backgroundColor = colourVal;
-
+    box.style.backgroundColor = noteColour;
+    
     // Self explanatory
     document.getElementsByClassName('container')[0].appendChild(box);
-    // Insert the textNode into the current .box element (we know it's the current element because we've counted how many .box elements there are using count, then used count to select the index of the current element)
-    document.getElementsByClassName('box')[count].appendChild(text);
 
-    return;
+    // Insert the textNode into the current .box element (we know it's the current element because we've counted how many .box elements there are using count, then used count to select the index of the current element)
+    document.getElementsByClassName('box')[count].appendChild(textNode);
+
 }
 
 document.getElementById('make-sticky').onclick = makeNote; // Make the note
