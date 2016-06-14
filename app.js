8e22f899ca52aeae56ba1c6263a73cc50e429eb1
Marcel Jacobs <silent.party@gmail.com>
2016-06-14 17:17:20 +1000

    removed old commented-out code

--- app.js
+++ app.js
@@ -27,29 +27,6 @@ function makeNote() {
     document.getElementsByClassName('box')[count].appendChild(text);
 
     return;
-};
+}
 
 document.getElementById('make-sticky').onclick = makeNote; // Make the note
-
-
-/*
-// Quick n' dirty
-
-var count = 0;
-document.getElementById('make-sticky').onclick = function() {
-  var box = document.createElement("box");
-  var p = document.createElement("p");
-  var colourVal = document.getElementById('colour').value;
-  var textVal = document.getElementById('text').value;
-  var text = document.createTextNode(count + "-" + textVal);
-
-  box.className = "box";
-  box.style.backgroundColor = colourVal;
-
-  document.getElementsByClassName('container')[0].appendChild(box);
-  document.getElementsByClassName('box')[count].appendChild(text);
-
-  count++;
-
-};
-*/
